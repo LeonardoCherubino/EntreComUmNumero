b39faa9c100e6a7825a73c884197aa9cd77343f7
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-07-24 20:24:20 -0300

    Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,3 @@
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+Numero = int(input('Entre com um número: '))
+print('O número digitado foi: {}'.format(Numero))
